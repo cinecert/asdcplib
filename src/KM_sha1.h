d9757786bb5a2d7a6cc9bce7cfb1a8b52d6391cf
jhurst <jhurst@cinecert.com>
2020-07-15 13:11:05 -0700

    Moving sha1 into the library to reduce openssl dependency

---
+++ src/KM_sha1.h
@@ -0,0 +1,54 @@
+/*
+Copyright (c) 2006-2020, John Hurst
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the author may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+  /*! \file    KM_sha1.h
+    \version $Id$
+    \brief   SHA-1 message digest
+  */
+
+#ifndef _KM_SHA1_H_
+#define _KM_SHA1_H_
+
+#include <KM_util.h>
+
+namespace Kumu {
+
+  typedef struct {
+    uint32_t state[5];
+    uint32_t count[2];
+    byte_t buffer[64];
+  } SHA1_CTX;
+
+  void SHA1_Init(SHA1_CTX* context);
+  void SHA1_Update(SHA1_CTX* context, const byte_t* data, unsigned int len);
+  void SHA1_Final(byte_t digest[20], SHA1_CTX* context);
+}
+
+#endif // _KM_SHA1_H_
+
+//
+// end
+//
